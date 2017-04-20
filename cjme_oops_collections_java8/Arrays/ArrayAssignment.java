ae989a917ae3eff65d81198b28b81cb81468253f
Salma Shaik <salmashaik1611@gmail.com>
2017-04-20 14:34:41 +0530

    Arrays Demo

---
+++ cjme_oops_collections_java8/Arrays/ArrayAssignment.java
@@ -0,0 +1,8 @@
+public class ArrayAssignment{
+	public static void main(String[] args){
+		String[] strArray = {"Hi","How","are","you"};
+		for(int i=(strArray.length)-1;i>=0;i--){
+			System.out.println(strArray[i]);
+		}
+	}
+}
---
+++ cjme_oops_collections_java8/Arrays/ArraysDemo.java
@@ -0,0 +1,11 @@
+public class ArraysDemo{
+	public static void main(String[] args){
+		//int[] arr = {23, 35, 79};
+		int[] arr = new int[4];
+		arr[0]=23;
+		System.out.println(arr[0]);
+		//for(int i=0;i<arr.length;i++){
+		//	System.out.println(i);
+		//}
+	}
+}
